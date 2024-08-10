bba7c0531cc97cc4261e1e8f3f098ab199acf6f9
Aluminatix <p4as3nj85@gmail.com>
2024-08-11 03:36:36 +0530

    Frontend Complete

---
+++ src/components/shared/UserItem.jsx
@@ -0,0 +1,59 @@
+import { Avatar, IconButton, ListItem, Stack, Typography } from '@mui/material';
+import React, {memo} from 'react'
+import {Add as AddIcon, Remove as RemoveIcon} from '@mui/icons-material';
+import { transformImage } from '../../lib/features';
+
+const UserItem = ({user, handler, hadlerIsLoading, isAdded=false, styling = {}, }) => {
+
+    const {name,_id,avatar} = user;
+
+  return (
+    <ListItem>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          spacing={"1rem"}
+          width={"100%"}
+          {...styling}
+
+        >
+            <Avatar src={transformImage(avatar)}/>
+
+            <Typography
+             variant='body1'
+             sx={{
+                flexGlow: 1,
+                display: "-webkit-box",
+                WebkitBoxOrient: "vertical",
+                WebkitLineClamp: 1,
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                width: "100%",
+             }}
+            > 
+
+            {name} 
+            
+            </Typography>
+            
+            <IconButton 
+              size='small'
+              sx={{
+                bgcolor: isAdded ? "error.main" : "primary.main",
+                color: "white",
+                "&:hover": {
+                    bgcolor: isAdded ? "error.dark" : "primary.dark",
+                },
+              }}
+              onClick={() => handler(_id)} 
+              disabled={hadlerIsLoading}>
+              
+              {isAdded ? <RemoveIcon/> : <AddIcon />}
+            
+            </IconButton>
+        </Stack>
+    </ListItem>
+  );
+};
+
+export default memo(UserItem);
---
+++ src/constants/sampleData.js
@@ -0,0 +1,79 @@
+
+export const sampleChats = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Doe",
+    _id: "1",
+    groupChat: false,
+    members: ["1","2"],
+  },
+
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John oe",
+    _id: "2",
+    groupChat: true,
+    members: ["1","2"],
+  }
+];
+
+export const sampleUsers = [
+  {
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    name: "Pinky",
+    _id: "1",
+  },
+  {
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    name: "Rinky",
+    _id: "2",
+  },
+];
+
+export const sampleNotifications = [
+  {
+    sender: {
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      name: "Alex",
+    },
+    _id: "1",
+  },
+  {
+    sender: {
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      name: "Prafull",
+    },
+    _id: "2",
+  },
+];
+
+export const sampleMessage = [
+  {
+    attachments: [],
+    content: "L*uda ka Message hai",
+    _id: "sfnsdjkfsdnfkjsbnd",
+    sender: {
+      _id: "user._id",
+      name: "Chaman ",
+    },
+    chat: "chatId",
+    createdAt: "2024-02-12T10:41:30.630Z",
+  },
+
+  {
+    attachments: [
+      {
+        public_id: "asdsad 2",
+        url: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    ],
+    content: "",
+    _id: "sfnsdjkfsdnfkdddjsbnd",
+    sender: {
+      _id: "sdfsdfsdf",
+      name: "Chaman2",
+    },
+    chat: "chatId",
+    createdAt: "2024-02-12T10:41:30.630Z",
+  },
+];
